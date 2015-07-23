b4384beb7b826e7d6eafc890238d0a98e16b540e
Jared Hensley <jahensle85@me.com>
2015-07-23 18:20:30 -0400

    strikethrough to all

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function(){
-$('i.fa.fa-square-o.fa-2x').click(function () {
-	$('i.fa.fa-square-o.fa-2x').hide();
-		$("i.fa.fa-check-square-o.fa-2x").show();
 
+$('i.fa.fa-square-o.fa-2x').click(function () {
+	$('i.fa.fa-square-o.fa-2x').toggle();
+		$("i.fa.fa-check-square-o.fa-2x").toggle();
+		$("li").toggleClass('strikethrough');
+	});
+
+$("i.fa.fa-check-square-o.fa-2x").click(function () {
+	$('i.fa.fa-square-o.fa-2x').toggle();
+	$("i.fa.fa-check-square-o.fa-2x").toggle();
+	$("li").toggleClass('strikethrough');
 	});
 
 });
