a5b9a7a8a0f40e5debb98e27632c3b8ea6e0151c
Jared Hensley <jahensle85@me.com>
2015-07-23 18:26:39 -0400

    syntax

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 
 $(document).ready(function(){
 
-$('i.fa.fa-square-o.fa-2x').click(function () {
-	$('i.fa.fa-square-o.fa-2x').toggle();
+	$('i.fa.fa-square-o.fa-2x').click(function () {
+		$('i.fa.fa-square-o.fa-2x').toggle();
 		$("i.fa.fa-check-square-o.fa-2x").toggle();
 		$("li").toggleClass('strikethrough');
 	});
 
-$("i.fa.fa-check-square-o.fa-2x").click(function () {
-	$('i.fa.fa-square-o.fa-2x').toggle();
-	$("i.fa.fa-check-square-o.fa-2x").toggle();
-	$("li").toggleClass('strikethrough');
+	$("i.fa.fa-check-square-o.fa-2x").click(function () {
+		$('i.fa.fa-square-o.fa-2x').toggle();
+		$("i.fa.fa-check-square-o.fa-2x").toggle();
+		$("li").toggleClass('strikethrough');
 	});
-
 });
