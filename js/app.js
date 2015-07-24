7256444c7b02a471d09fc21c1514100f6607c4ea
Jared Hensley <jahensle85@me.com>
2015-07-24 16:24:42 -0400

    more

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@
 $(document).ready(function(){
 
 	$('i.fa.fa-square-o.fa-2x').click(function () {
-	/*	$(this).toggle();*/
+		$(this).toggle();
 		$(this).prev("i").toggleClass("fa fa-check-square-o fa-2x");
 		$(this).closest("li").toggleClass('strikethrough');
-		var listItem = $(this).closest("li");
+		/*var listItem = $(this).closest("li");*/
+	});
 
+	$('i.fa.fa-check-square-o.fa-2x').click(function() {
+		alert('test');
+		$(this).toggle();
+		/*$(this).next("i").toggleClass("fa fa-square-o fa-2x");*/
 
 	});
 
