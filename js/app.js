2c0d3dce9039be4b5e19e3b71fe17a305e1514de
Jared Hensley <jahensle85@me.com>
2015-07-24 13:55:07 -0400

    trashcan

--- js/app.js
+++ js/app.js
@@ -2,10 +2,23 @@
 $(document).ready(function(){
 
 	$('i.fa.fa-square-o.fa-2x').click(function () {
-		$(this).toggle();
+	/*	$(this).toggle();*/
 		$(this).prev("i").toggleClass("fa fa-check-square-o fa-2x");
 		$(this).closest("li").toggleClass('strikethrough');
+		var listItem = $(this).closest("li");
+
+
 	});
+
+	$('i.fa.fa-trash-o.trashspace.fa-2x').click(function () {
+		$(this).closest("li").remove();
+	});
+
+	/*$('i.fa.fa-check-square-o.fa-2x').click(function () {
+		$(this).toggle();
+		$(this).next("i").toggleClass("fa fa-square-o fa-2x");
+		$(this).closest("li").toggleClass('strikethrough');
+	});*/
 
 	/*$("i.fa.fa-check-square-o.fa-2x").click(function () {
 		$('i.fa.fa-square-o.fa-2x').toggle();
