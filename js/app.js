8c9f7af139a7dec1bc0b3e3ca0e7aea1be5c1c1a
Jared Hensley <jahensle85@me.com>
2015-07-24 20:03:34 -0400

    sanity

--- js/app.js
+++ js/app.js
@@ -1,33 +1,51 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$('i.fa.fa-square-o.fa-2x').click(function () {
-		$(this).toggle();
-		$(this).prev("i").toggleClass("fa fa-check-square-o fa-2x");
-		$(this).closest("li").toggleClass('strikethrough');
-		/*var listItem = $(this).closest("li");*/
-	});
+      $('ul').on('click', 'i', function() {
+        console.log(this);
+        var i = $(this);
 
-	$('i.fa.fa-check-square-o.fa-2x').click(function() {
-		alert('test');
-		$(this).toggle();
-		/*$(this).next("i").toggleClass("fa fa-square-o fa-2x");*/
+        if ((i).hasClass('fa fa-square-o fa-2x')) {
+          $(i).removeClass('fa fa-square-o fa-2x');
+          $(i).addClass('fa fa-check-square-o fa-2x');
 
-	});
+        } else {
+          $(i).removeClass('fa fa-check-square-o fa-2x');
+          $(i).addClass('fa fa-square-o fa-2x');
 
-	$('i.fa.fa-trash-o.trashspace.fa-2x').click(function () {
+        }
+        $(i).closest("li").toggleClass('strikethrough');
+      });
+
+
+	$('i.fa.fa-trash-o.trashspace.fa-2x').on('click', function () {
 		$(this).closest("li").remove();
 	});
 
-	/*$('i.fa.fa-check-square-o.fa-2x').click(function () {
-		$(this).toggle();
-		$(this).next("i").toggleClass("fa fa-square-o fa-2x");
-		$(this).closest("li").toggleClass('strikethrough');
-	});*/
-
-	/*$("i.fa.fa-check-square-o.fa-2x").click(function () {
-		$('i.fa.fa-square-o.fa-2x').toggle();
-		$("i.fa.fa-check-square-o.fa-2x").toggle();
-		$("li").toggleClass('strikethrough');
-	});*/
+	
 });;
+
+
+
+/*$(document).ready(function(){
+
+	$('i.fa.fa-square-o.fa-2x').on('click', function () {
+		$(this).toggle();
+		$(this).prev("i").toggleClass("fa fa-check-square-o fa-2x");
+		$(this).closest("li").toggleClass('strikethrough');
+		//var listItem = $(this).closest("li");
+	});
+
+	$('ul').on('click','i.fa.fa-check-square-o.fa-2x', function() {
+		
+		$(this).toggle();
+		$(this).next().toggle();
+		$(this).closest("li").toggleClass('strikethrough');
+	});
+
+	$('i.fa.fa-trash-o.trashspace.fa-2x').on('click', function () {
+		$(this).closest("li").remove();
+	});
+
+	
+});;*/
