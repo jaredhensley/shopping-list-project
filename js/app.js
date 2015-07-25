1127bb6e3bc5233325c46fa09901a08206e74447
Jared Hensley <jahensle85@me.com>
2015-07-25 17:40:04 -0400

    working

--- js/app.js
+++ js/app.js
@@ -9,32 +9,37 @@ $(document).ready(function() {
           $(i).removeClass('fa fa-square-o fa-2x');
           $(i).addClass('fa fa-check-square-o fa-2x');
 
-        } else {
+        } else if ((i).hasClass('trashcan')) {
+      		$(this).parents("li").remove();	
+
+      	} else {
           $(i).removeClass('fa fa-check-square-o fa-2x');
           $(i).addClass('fa fa-square-o fa-2x');
-
         }
-        $(i).closest("li").toggleClass('strikethrough');
 
-      
+        $(i).closest("li").toggleClass('strikethrough');
+      	
       });
 
 
-			$('ul').on('click', 'i.trashcan', function () {
+		/*	$('ul').on('click', 'i.trashcan', function () {
 				$(this).parents("li").remove();
-			});
+			});*/
 
 			$('button').on('click', function(event) {
 				event.preventDefault();
 				var checkbox = '<i class="fa fa-square-o fa-2x"></i>';
-				var trashcan = ' <i class="fa fa-trash-o trashspace fa-2x"></i>';
+				var trashcan = ' <i class="fa fa-trash-o trashspace fa-2x trashcan"></i>';
 				var input = $("input[name='item_name']").val();
 				$('ul').append('<li>' + input + '<span>' + checkbox + trashcan + '</li>');
 
 			});
 
-	
 });;
 
-		
+
+
+
+
+
 
