55bb91408f7396cf575a14e972b26a1c35e229c8
Jared Hensley <jahensle85@me.com>
2015-07-25 20:02:29 -0400

    almost done

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 
 $(document).ready(function() {
 
-      $('ul').on('click', 'i', function() {
-        console.log(this);
-        var i = $(this);
+      $('ul').on('click', 'i', function() {  // if you click an <i> element in the DOM
+        var i = $(this);  // setting a variable referenced to the clicked element
 
-        if ((i).hasClass('fa fa-square-o fa-2x')) {
+        if ((i).hasClass('fa fa-square-o fa-2x')) { // if user clicks empty square
           $(i).removeClass('fa fa-square-o fa-2x');
           $(i).addClass('fa fa-check-square-o fa-2x');
 
-        } else if ((i).hasClass('trashcan')) {
+        } else if ((i).hasClass('trashcan')) { //if user clicks trashcan, do the following
       		$(this).parents("li").remove();	
 
-      	} else {
-          $(i).removeClass('fa fa-check-square-o fa-2x');
+      	} else { // if user clicks checked square
+          $(i).removeClass('fa fa-check-square-o fa-2x'); 
           $(i).addClass('fa fa-square-o fa-2x');
         }
 
@@ -27,13 +26,14 @@ $(document).ready(function() {
 			});*/
 
 			$('button').on('click', function(event) {
-				event.preventDefault();
+				event.preventDefault(); // prevents page reload after submission 
 				var checkbox = '<i class="fa fa-square-o fa-2x"></i>';
-				var trashcan = ' <i class="fa fa-trash-o trashspace fa-2x trashcan"></i>';
-				var input = $("input[name='item_name']").val();
-				$('ul').append('<li>' + input + '<span>' + checkbox + trashcan + '</li>');
-
+				var trashcan = ' <i class="fa fa-trash-o trashspace fa-2x trashcan"></i>'; 
+				var input = $("input[name='item_name']").val(); // creating a variable to input value
+				$('ul').append('<li>' + input + '<span>' + checkbox + trashcan + '</li>'); // appends new list item
+				$('#item').val(''); // resets input field
 			});
+
 
 });;
 
